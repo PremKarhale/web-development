b8c9f7d35bcc86c08e3de07b4b67d6ebffd86409
Prem Karhale <premkarhale383@gmail.com>
2025-02-19 19:44:11 +0530

    Java Script practice 1

---
+++ HTMLPRACTICE35/script.js
@@ -0,0 +1,13 @@
+alert("hello world ");
+console.log("code is running....");
+console.log("code is also running....");
+
+var a= prompt("Enter your no ")
+var isTrue= confirm("Are you shure that you want to do shutting down your computer...")
+
+if(isTrue){
+    console.log("computer is shutting down ...")
+}
+else{
+    console.log("you saved your computer from shutting down ..")
+}
