cda3e1495c3fb2228dbf003c9b6a6c510ddfa05d
Prem Karhale <premkarhale383@gmail.com>
2025-02-22 11:10:43 +0530

    Keyword var , let , const  & creation of object ;

---
+++ Javascript_practice2/script.js
@@ -0,0 +1,34 @@
+console.log("hey this was my starting of java script");
+console.log("hello world");
+var a=6;
+var b=5;
+var c=4;
+let f=7;
+var _a="harry";
+var _b=_a + b;
+console.log(_b);
+
+a=a+2;
+{
+    let a=67;
+    console.log(a)
+}
+console.log(a+b+c);
+console.log(typeof a,typeof b,typeof c,typeof _a);
+// const a1=5;
+// a1=a1+4;
+// this is not possible;
+
+let o={
+name:"harry",
+    "job role":"softwere engineer",
+    "job code": "455"
+}
+console.log(o);
+o.salary="100 crores";
+console.log(o);
+
+
+
+ 
+
