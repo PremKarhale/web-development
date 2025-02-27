3f80eae1464cfe2e8a272e3f85f34395c4f6825f
Prem Karhale <premkarhale383@gmail.com>
2025-02-27 21:19:55 +0530

    Java script practice if/else practice

---
+++ Javascript_practice3/script.js
@@ -0,0 +1,30 @@
+let age = 0;
+if (age == 18) {
+    console.log("you can't drive");
+}
+else if (age == 0) {
+    console.log("hey are you kidding .")
+
+}
+else {
+    console.log("you can drive");
+}
+
+
+    let marks = 46;
+    let result=(marks > 45) ? 'pass' : 'fail';
+    console.log(result);
+{
+    let  a = 5;
+    let  b = 6;
+    
+    let  c = a > b ? a + b : a - b;
+    console.log(c)
+}
+ 
+
+
+
+
+
+
