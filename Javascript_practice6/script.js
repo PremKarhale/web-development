5fd4e134eb31911c11061be4caf85f16cc87a468
Prem Karhale <premkarhale383@gmail.com>
2025-02-28 20:12:47 +0530

    Properties in javascript

---
+++ Javascript_practice6/script.js
@@ -0,0 +1,20 @@
+console.log("write your name :")
+let name="hari"
+let b=" Dnyaneshwar"
+console.log(name)
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[3])
+console.log(name.length)
+
+console.log("his name is "+ name+ " karhale")
+console.log(`you know his name was ${name} and his surname was karhale`);
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(0,3))
+console.log(name.slice(0))
+console.log(name.replace("pre","001"))
+console.log(name.concat(b," karhale"));
+
+
