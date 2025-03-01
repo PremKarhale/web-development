31186a4e192a1bae6d9393623098ab059293a52a
Prem Karhale <premkarhale383@gmail.com>
2025-03-01 12:27:06 +0530

    operation

---
+++ Javascript_practice5/script.js
@@ -0,0 +1,19 @@
+function nice(name) {
+    console.log("Hey "+ name +" you are nice !")
+    console.log("Hey "+ name +" you are good in web dev")  
+}
+nice("prem")
+function product(a,b){
+   return a*b;
+}
+result =product(3,4)
+// console.log(3,4)
+console.log("product of two no are :" ,result);
+
+
+function sum(a,b) {
+    return a+b;
+}
+// sum(3,4)
+result=sum(3,5);
+console.log("sum of the two no are: " ,result)
