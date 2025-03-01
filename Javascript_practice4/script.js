c0d54c3ffcb21ccb940c5b180883fcba467c9567
Prem Karhale <premkarhale383@gmail.com>
2025-03-01 12:20:56 +0530

    loops

--- Javascript_practice4/script.js
+++ Javascript_practice4/script.js
@@ -1,10 +1,11 @@
 console.log("practice of all the loops used in the javascript ");
 
-
+// FOR LOOP
 for (let a = 0; a < 10.; a++) {
     console.log(a);
     
 }
+// FORIN LOOP
 let obj={
     name:":"+" prem",
     role:"softwhere engineer",
@@ -15,6 +16,7 @@ for (const key in obj) {
     const element = obj[key];
     console.log(key,element)
 }
+// WHILE LOOP
 
 let i=1;
 while (i<10) {
@@ -24,6 +26,7 @@ while (i<10) {
 }
 console.log(" ");
 
+// DO WHILE LOOP 
 let b=0;
 do {
     console.log(b)
