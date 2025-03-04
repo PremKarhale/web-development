90a4b4dba1cf0c35180648852b0e987c90adc95e
Prem Karhale <premkarhale383@gmail.com>
2025-03-05 00:36:47 +0530

    Properties of an arrays

--- javascript_practice8/script1.js
+++ javascript_practice8/script1.js
@@ -1,7 +1,26 @@
 let a=[23,44,56,78,36,72,30]
 
-for (let i = 0; i < a.length; i++) {
-    const element = a[i];
-    console.log(element);
+// for (let i = 0; i < a.length; i++) {
+//     const element = a[i];
+//     console.log(element);
     
+// }
+// for (const element of a) {
+//   console.log(element)
+    
+// }
+//  let newarray=[];
+
+// for (let i = 0; i < a.length; i++) {
+//     const element = a[i];
+//     newarray.push(element**2)
+
+// }
+// console.log(newarray)
+
+let newarr =a.map((e)=>{  //map properties  map()
+    return e**2;
 }
+)
+
+console.log(newarr)
