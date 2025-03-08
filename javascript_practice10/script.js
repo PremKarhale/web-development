dca1ab329805b6bf0d8b268afed33b93c43737a6
Prem Karhale <premkarhale383@gmail.com>
2025-03-08 23:26:29 +0530

    To find the factorial of the given no using for loop.

---
+++ javascript_practice10/script.js
@@ -0,0 +1,15 @@
+//dTo find the factorial of the given no using for loop.
+
+let a=6
+    for (let i = 5; i >=0; i--) {
+        const element = [i];
+        a=a*i;
+        console.log("6"+"*"+i+"="+a);
+        
+    }
+
+
+
+    
+
+
