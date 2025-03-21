d3c133779ec2068370499e12f504b4cce3381852
Prem Karhale <premkarhale383@gmail.com>
2025-03-21 23:54:02 +0530

    Event Listner

---
+++ javascript_practice16/script.js
@@ -0,0 +1,10 @@
+let button = document.getElementById("btn");
+button.addEventListener("dblclick",()=>{
+    document.querySelector(".box").innerHTML="<b> hey iam the box here.</b>"
+
+})
+// let button = document.getElementById("btn");
+// button.addEventListener("click",()=>{
+//    alert("hey the box is clicked by you ")
+
+// })
