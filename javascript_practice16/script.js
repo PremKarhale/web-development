285bc907d60b408324ac36c2dbe42240bf5cb4d1
Prem Karhale <premkarhale383@gmail.com>
2025-03-22 00:23:14 +0530

    Even Listner

--- javascript_practice16/script.js
+++ javascript_practice16/script.js
@@ -1,8 +1,14 @@
 let button = document.getElementById("btn");
-button.addEventListener("dblclick",()=>{
-    document.querySelector(".box").innerHTML="<b> hey iam the box here.</b>"
+ button.addEventListener("dblclick",()=>{
+    let box=document.querySelector(".box");
+   if (box.innerHTML.includes("CLICK ME TWICE")) {
+    box.innerHTML="<b> hey i am the box here </b>"
+   }else{
+    box.innerHTML="CLICK ME TWICE"
+   }
 
 })
+
 // let button = document.getElementById("btn");
 // button.addEventListener("click",()=>{
 //    alert("hey the box is clicked by you ")
