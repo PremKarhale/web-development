f8fd1b5386853c6e75836a40973573109b13e2d6
Prem Karhale <premkarhale383@gmail.com>
2025-03-29 18:22:11 +0530

    Event Listner

--- javascript_practice16/script.js
+++ javascript_practice16/script.js
@@ -1,16 +1,18 @@
-let button = document.getElementById("btn");
- button.addEventListener("dblclick",()=>{
-    let box=document.querySelector(".box");
-   if (box.innerHTML.includes("CLICK ME TWICE")) {
-    box.innerHTML="<b> hey i am the box here </b>"
-   }else{
-    box.innerHTML="CLICK ME TWICE"
-   }
-
-})
 
 // let button = document.getElementById("btn");
 // button.addEventListener("click",()=>{
 //    alert("hey the box is clicked by you ")
 
 // })
+
+
+let button = document.getElementById("btn");
+ button.addEventListener("click",()=>{
+    let box=document.querySelector(".box");
+   if (box.innerHTML.includes("CLICK ME ONCE")) {
+    box.innerHTML="<b> hey i am the box here </b>"
+   }else{
+    box.innerHTML="CLICK ME ONCE"
+   }
+
+})
