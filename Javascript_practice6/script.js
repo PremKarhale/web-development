ab7bbb8b1b4483a692f9296c767dae32f29c33a5
Prem Karhale <premkarhale383@gmail.com>
2025-03-30 22:31:03 +0530

    >properties (uppercase, lowercase, slice , replace, concat)

--- Javascript_practice6/script.js
+++ Javascript_practice6/script.js
@@ -2,7 +2,7 @@
 
 console.log("write your name :")
 let name="prem"
-let b=" Dnyaneshwar"
+let b=" karhale"
 console.log(name)
 console.log(name[0])
 console.log(name[1])
@@ -11,12 +11,13 @@ console.log(name[3])
 console.log(name.length)
 
 console.log("his name is "+ name+ " karhale")
-console.log(`you know his name was ${name} and his surname was karhale`);
+console.log(`you know his name was ${name} and his surname was ${b}`);
 console.log(name.toUpperCase())
 console.log(name.toLowerCase())
+console.log(name.slice(0,0))  //it slices all the element in the name . o r we can also write it as (name.length-4)
 console.log(name.slice(0,3))
 console.log(name.slice(0))
 console.log(name.replace("pre","001"))
-console.log(name.concat(b," karhale"));
+console.log(name.concat(b));
 
 
